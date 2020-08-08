de9abb977a19b87ab5a7256a872ce1e223693aff
LeKAKiD <lekakid@kakao.com>
2020-08-08 12:29:11 +0900

    조회 중인 게시물이 최상단으로 올라오는 현상 수정

--- src/module/refresher.js
+++ src/module/refresher.js
@@ -26,7 +26,7 @@ function getRefreshArticle() {
         req.open('GET', window.location.href);
         req.responseType = 'document';
         req.addEventListener('load', () => {
-            const articles = req.response.querySelectorAll('a[class="vrow"]');
+            const articles = req.response.querySelectorAll('a[class="vrow"], a.vrow.active');
             resolve(articles);
         });
         req.send();
@@ -35,7 +35,7 @@ function getRefreshArticle() {
 
 function swapNewArticle(newArticles) {
     const board = document.querySelector('.board-article-list .list-table, .included-article-list .list-table');
-    const oldArticles = board.querySelectorAll('a[class="vrow"]');
+    const oldArticles = board.querySelectorAll('a[class="vrow"], a.vrow.active');
 
     let i = newArticles.length - 1;
     const oldnum = parseInt(oldArticles[0].pathname.split('/')[3], 10);
