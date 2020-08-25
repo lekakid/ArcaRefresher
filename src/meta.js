e8799ede3cbc625a56ab4b2253a60a52688f4cdb
LeKAKiD <lekakid@kakao.com>
2020-08-25 23:24:35 +0900

    스크립트 실행 타이밍 변경

--- src/meta.js
+++ src/meta.js
@@ -1,19 +1,21 @@
 // ==UserScript==
-// @name        Arca Refresher
-// @namespace   LeKAKiD
-// @description Arca Live Extension
-// @downloadURL https://github.com/lekakid/ArcaRefresher/releases/latest/download/ArcaRefresher.user.js
-// @homepageURL https://github.com/lekakid/ArcaRefresher
-// @supportURL  https://arca.live/b/namurefresher
-// @match       https://*.arca.live/*
-// @match       https://arca.live/*
+// @name            Arca Refresher
+// @namespace       LeKAKiD
+// @description     Arca Live Extension
+// @downloadURL     https://github.com/lekakid/ArcaRefresher/releases/latest/download/ArcaRefresher.user.js
+// @homepageURL     https://github.com/lekakid/ArcaRefresher
+// @supportURL      https://arca.live/b/namurefresher
+// @match           https://*.arca.live/*
+// @match           https://arca.live/*
+// @exclude-match   https://st*.arca.live/*
 // @noframes
-// @grant       GM_setValue
-// @grant       GM_getValue
-// @grant       GM_xmlhttpRequest
-// @version     process.env.VERSION
-// @author      LeKAKiD
-// @require     https://unpkg.com/file-saver@2.0.2/dist/FileSaver.min.js
-// @require     https://unpkg.com/jszip@3.1.5/dist/jszip.min.js
-// @require     https://cdn.jsdelivr.net/combine/npm/@violentmonkey/dom,npm/@violentmonkey/ui
+// @run-at          document-start
+// @grant           GM_setValue
+// @grant           GM_getValue
+// @grant           GM_xmlhttpRequest
+// @version         process.env.VERSION
+// @author          LeKAKiD
+// @require         https://unpkg.com/file-saver@2.0.2/dist/FileSaver.min.js
+// @require         https://unpkg.com/jszip@3.1.5/dist/jszip.min.js
+// @require         https://cdn.jsdelivr.net/combine/npm/@violentmonkey/dom,npm/@violentmonkey/ui
 // ==/UserScript==
