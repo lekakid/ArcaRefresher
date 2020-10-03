8715f7be8a79dd52af7cfba8dc7c0c6092d2c9d5
LeKAKiD <lekakid@kakao.com>
2020-10-03 18:43:17 +0900

    스타일: 코드 순서 변경

--- src/module/LiveModifier.js
+++ src/module/LiveModifier.js
@@ -6,18 +6,20 @@ export default { apply };
 
 function apply() {
     document.head.append(<style>{sheetLiveModifier}</style>);
-
-    const fixHeader = GM_getValue('fixHeader', DefaultConfig.fixHeader);
-    const hideAvatar = GM_getValue('hideAvatar', DefaultConfig.hideAvatar);
-    const hideMedia = GM_getValue('hideMedia', DefaultConfig.hideMedia);
-    const hideModified = GM_getValue('hideModified', DefaultConfig.hideModified);
-    const hideSideMenu = GM_getValue('hideSideMenu', DefaultConfig.hideSideMenu);
-
     const contentWrapper = document.querySelector('.content-wrapper');
 
+    const fixHeader = GM_getValue('fixHeader', DefaultConfig.fixHeader);
     if(fixHeader) document.body.classList.add('fix-header');
+
+    const hideAvatar = GM_getValue('hideAvatar', DefaultConfig.hideAvatar);
     if(hideAvatar) contentWrapper.classList.add('hide-avatar');
+
+    const hideMedia = GM_getValue('hideMedia', DefaultConfig.hideMedia);
     if(hideMedia) contentWrapper.classList.add('hide-media');
+
+    const hideModified = GM_getValue('hideModified', DefaultConfig.hideModified);
     if(hideModified) contentWrapper.classList.add('hide-modified');
+
+    const hideSideMenu = GM_getValue('hideSideMenu', DefaultConfig.hideSideMenu);
     if(hideSideMenu) contentWrapper.classList.add('hide-sidemenu');
 }
