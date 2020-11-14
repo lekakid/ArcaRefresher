2ba58b25cf05626a8d1a7ac322f1093d1e389ec6
LeKAKiD <lekakid@kakao.com>
2020-11-15 01:20:05 +0900

    개선: 이미지 타입 변환 코드 분리

--- src/util/DownloadManager.js
+++ src/util/DownloadManager.js
@@ -15,3 +15,21 @@ export function getBlob(url, onProgress, onLoad) {
         });
     });
 }
+
+export function getArrayBuffer(url, onProgress, onLoad) {
+    return new Promise((resolve, reject) => {
+        GM_xmlhttpRequest({
+            method: 'GET',
+            url,
+            responseType: 'arraybuffer',
+            onprogress: event => {
+                if(onProgress) onProgress(event);
+            },
+            onload: response => {
+                if(onLoad) onLoad();
+                resolve(response.response);
+            },
+            onerror: reject,
+        });
+    });
+}
