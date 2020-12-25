b5238b1ed9581367193d48a48e5e083651f4b480
LeKAKiD <lekakid@kakao.com>
2020-12-25 16:01:37 +0900

    개선: ArticleMenu 모듈 개선
    미사용 코드 삭제
    코드 간략화

--- src/core/ArticleMenu.js
+++ src/core/ArticleMenu.js
@@ -2,22 +2,20 @@ import Parser from './Parser';
 
 export default { appendMenuBtn };
 
-function appendMenuBtn(name, icon, title, event) {
+function appendMenuBtn(name, icon, title) {
     const headerMenu = Parser.queryView('article').querySelector('.edit-menu');
     if(!headerMenu) return;
 
     const element = (
-        <a href="#" title={title}>
-            {icon && <span class={icon} />}
-            {` ${name}`}
-        </a>
+        <>
+            <a href="#" title={title}>
+                {icon && <span class={icon} />}
+                {` ${name}`}
+            </a>
+            {headerMenu.childElementCount && <span class="sep" />}
+        </>
     );
 
-    element.addEventListener('click', event);
-
-    if(headerMenu.childElementCount) {
-        headerMenu.prepend(<span class="sep" />);
-    }
     headerMenu.prepend(element);
     return element;
 }
