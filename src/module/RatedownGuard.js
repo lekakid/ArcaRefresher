7ed45d3f575c840dc5b735b4b5d14b3c06b69782
LeKAKiD <lekakid@kakao.com>
2020-12-26 01:40:47 +0900

    RatedownGuard 설정 포맷 수정

--- src/module/RatedownGuard.js
+++ src/module/RatedownGuard.js
@@ -3,38 +3,32 @@ import Configure from '../core/Configure';
 export default { addSetting, apply };
 
 const RATEDOWN_GUARD = 'blockRatedown';
-const RATEDOWN_GUARD_DEFAULT = false;
 
 function addSetting() {
-    const settingElement = (
-        <>
-            <label class="col-md-3">비추천 방지</label>
-            <div class="col-md-9">
-                <select>
-                    <option value="false">사용 안 함</option>
-                    <option value="true">사용</option>
-                </select>
-                <p>비추천 버튼을 클릭하면 다시 한 번 확인창을 띄웁니다.</p>
-            </div>
-        </>
+    const ratedownBlock = (
+        <select>
+            <option value="false">사용 안 함</option>
+            <option value="true">사용</option>
+        </select>
     );
-
-    const selectElement = settingElement.querySelector('select');
-
-    function load() {
-        const data = GM_getValue(RATEDOWN_GUARD, RATEDOWN_GUARD_DEFAULT);
-
-        selectElement.value = data;
-    }
-    function save() {
-        GM_setValue(RATEDOWN_GUARD, selectElement.value == 'true');
-    }
-
-    Configure.addSetting(settingElement, Configure.categoryKey.UTILITY, save, load);
+    Configure.addSetting({
+        category: Configure.categoryKey.UTILITY,
+        header: '비추천 방지',
+        option: ratedownBlock,
+        description: '비추천 버튼을 클릭하면 다시 한 번 확인창을 띄웁니다.',
+        callback: {
+            save() {
+                GM_setValue(RATEDOWN_GUARD, ratedownBlock.value == 'true');
+            },
+            load() {
+                ratedownBlock.value = GM_getValue(RATEDOWN_GUARD, false);
+            },
+        },
+    });
 }
 
 function apply() {
-    if(!GM_getValue(RATEDOWN_GUARD, RATEDOWN_GUARD_DEFAULT)) return;
+    if(!GM_getValue(RATEDOWN_GUARD, false)) return;
 
     const ratedown = document.querySelector('#rateDown');
     if(ratedown == null) return;
