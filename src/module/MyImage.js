da8b76ac5a6944279c1e0394a93363e2f68eebe4
LeKAKiD <lekakid@kakao.com>
2020-12-26 02:10:13 +0900

    MyImage 설정 포맷 수정

--- src/module/MyImage.js
+++ src/module/MyImage.js
@@ -5,55 +5,47 @@ import Parser from '../core/Parser';
 export default { addSetting, addContextMenu, apply };
 
 const MY_IMAGES = 'myImages';
-const MY_IMAGES_DEFAULT = {};
 
 function addSetting() {
-    const settingElement = (
-        <>
-            <label class="col-md-3">자짤 관리</label>
-            <div class="col-md-9">
-                <select size="6" multiple="" />
-                <button name="delete" class="btn btn-arca">삭제</button>
-                <p>
-                    Ctrl, Shift, 마우스 드래그를 이용해서 여러개를 동시에 선택 할 수 있습니다.
-                </p>
-            </div>
-        </>
-    );
-
-    const selectElement = settingElement.querySelector('select');
-    const deleteBtn = settingElement.querySelector('button[name="delete"]');
+    const imgList = <select size="6" multiple="" />;
+    const deleteBtn = <button class="btn btn-arca">삭제</button>;
     deleteBtn.addEventListener('click', event => {
         event.target.disabled = true;
 
-        const removeElements = selectElement.selectedOptions;
+        const removeElements = imgList.selectedOptions;
         while(removeElements.length > 0) removeElements[0].remove();
 
         event.target.disabled = false;
     });
-
     const channel = Parser.getChannelInfo().id;
+    Configure.addSetting({
+        category: Configure.categoryKey.UTILITY,
+        header: '자짤 관리',
+        option: (
+            <>
+                {imgList}
+                {deleteBtn}
+            </>
+        ),
+        description: 'Ctrl, Shift, 마우스 드래그를 이용해서 여러개를 동시에 선택 할 수 있습니다.',
+        callback: {
+            save() {
+                const data = GM_getValue(MY_IMAGES, { [channel]: [] });
 
-    function load() {
-        const data = GM_getValue(MY_IMAGES, MY_IMAGES_DEFAULT)[channel];
-        if(!data) return;
+                const images = Array.from(imgList.children, e => e.value);
+                data[channel] = images;
+                GM_setValue(MY_IMAGES, data);
+            },
+            load() {
+                const data = GM_getValue(MY_IMAGES, {})[channel];
+                if(!data) return;
 
-        for(const i of data) {
-            selectElement.append(<option value={i}>{i}</option>);
-        }
-    }
-    function save() {
-        const data = GM_getValue(MY_IMAGES, MY_IMAGES_DEFAULT);
-        if(!data[channel]) {
-            data[channel] = [];
-        }
-
-        const images = Array.from(selectElement.children, e => e.value);
-        data[channel] = images;
-        GM_setValue(MY_IMAGES, data);
-    }
-
-    Configure.addSetting(settingElement, Configure.categoryKey.UTILITY, save, load);
+                for(const i of data) {
+                    imgList.append(<option value={i}>{i}</option>);
+                }
+            },
+        },
+    });
 }
 
 function addContextMenu() {
@@ -62,7 +54,7 @@ function addContextMenu() {
     addMyImageItem.addEventListener('click', event => {
         event.preventDefault();
 
-        const imgList = GM_getValue(MY_IMAGES, MY_IMAGES_DEFAULT);
+        const imgList = GM_getValue(MY_IMAGES, {});
         if(!imgList[channel]) {
             imgList[channel] = [];
         }
@@ -83,7 +75,7 @@ function addContextMenu() {
 function apply(editor) {
     const channel = Parser.getChannelInfo().id;
     if(editor.core.isEmpty()) {
-        const imgList = GM_getValue(MY_IMAGES, MY_IMAGES_DEFAULT)[channel];
+        const imgList = GM_getValue(MY_IMAGES, {})[channel];
         if(!imgList || !imgList.length) return;
 
         const img = imgList[Math.floor(Math.random() * imgList.length)];
