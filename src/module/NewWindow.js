bc440fd3c2695939a801048e8247bcd561dc5518
LeKAKiD <lekakid@kakao.com>
2020-12-26 02:07:03 +0900

    NewWindow 설정 포맷 수정

--- src/module/NewWindow.js
+++ src/module/NewWindow.js
@@ -7,33 +7,26 @@ const OPEN_NEW_WINDOW = 'openNewWindow';
 const OPEN_NEW_WINDOW_DEFAULT = false;
 
 function addSetting() {
-    const selectElement = (
+    const newWindow = (
         <select>
             <option value="false">사용 안 함</option>
             <option value="true">사용</option>
         </select>
     );
-
-    const settingElement = (
-        <>
-            <label class="col-md-3">게시물 새 창으로 열기</label>
-            <div class="col-md-9">
-                {selectElement}
-                <p>게시물 클릭 시 새창으로 띄워줍니다.</p>
-            </div>
-        </>
-    );
-
-    function load() {
-        const value = GM_getValue(OPEN_NEW_WINDOW, OPEN_NEW_WINDOW_DEFAULT);
-
-        selectElement.value = value;
-    }
-    function save() {
-        GM_setValue(OPEN_NEW_WINDOW, selectElement.value == 'true');
-    }
-
-    Configure.addSetting(settingElement, Configure.categoryKey.INTERFACE, save, load);
+    Configure.addSetting({
+        category: Configure.categoryKey.INTERFACE,
+        header: '게시물 새 창으로 열기',
+        option: newWindow,
+        description: '게시물 클릭 시 새창으로 띄워줍니다.',
+        callback: {
+            save() {
+                GM_setValue(OPEN_NEW_WINDOW, newWindow.value == 'true');
+            },
+            load() {
+                newWindow.value = GM_getValue(OPEN_NEW_WINDOW, false);
+            },
+        },
+    });
 }
 
 function apply() {
