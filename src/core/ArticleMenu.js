35a3a8eb2cf782e753eee1db31e6c3eccc615a5b
LeKAKiD <lekakid@kakao.com>
2021-01-13 20:48:19 +0900

    개선: ArticleMenu 파라미터 주석 추가

--- src/core/ArticleMenu.js
+++ src/core/ArticleMenu.js
@@ -2,8 +2,15 @@ import Parser from './Parser';
 
 export default { addHeaderBtn };
 
-function addHeaderBtn(buttonObject) {
-  const { text, icon, description, onClick } = buttonObject;
+/**
+ * 게시물 상단 헤더 메뉴에 버튼을 추가합니다.
+ * @param {Object} param                파라미터 오브젝트
+ * @param {string} param.text           버튼 텍스트
+ * @param {string} param.icon           버튼 좌측에 붙을 아이콘
+ * @param {string} [param.description]  버튼에 마우스를 올려두면 표시될 설명
+ * @param {Function} param.onClick      버튼을 클릭 시 호출할 콜백 함수
+ */
+function addHeaderBtn({ text, icon, description, onClick }) {
   const headerMenu = Parser.queryView('article').querySelector('.edit-menu');
 
   const element = (
