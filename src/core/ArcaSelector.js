dd338ab1e5766f61b3c8510062d5dfede64efb73
LeKAKiD <lekakid@kakao.com>
2021-05-16 22:17:23 +0900

    feat: 주사용 설렉터 분리

---
+++ src/core/ArcaSelector.js
@@ -0,0 +1,13 @@
+export const BOARD_VIEW =
+  'div.board-article-list .list-table, div.included-article-list .list-table';
+export const ARTICLE_VIEW = '.article-wrapper';
+export const ARTICLE_BODY = '.article-body';
+export const COMMENT_VIEW = '#comment';
+export const WRITE_VIEW = '.article-write';
+export const FOOTER_VIEW = 'footer';
+
+export const CHANNEL_TITLE = '.board-title a:not([class])';
+export const AUTHOR_INFO = '.article-head .user-info';
+export const CATEGORYS = '.board-category a';
+export const ARTICLES_ON_BOARD = 'a.vrow';
+export const NOT_NOTICE_ARTICLES_ON_BOARD = 'a.vrow:not(.notice)';
