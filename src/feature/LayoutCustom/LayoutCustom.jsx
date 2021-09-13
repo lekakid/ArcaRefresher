3ab0264591a98d6158c45e507503a0a45604aeeb
LeKAKiD <lekakid@kakao.com>
2021-09-13 18:17:22 +0900

    refactor: LayoutCustom 성능 개선, 코드 가독성 개선

--- src/feature/LayoutCustom/LayoutCustom.jsx
+++ src/feature/LayoutCustom/LayoutCustom.jsx
@@ -1,76 +1,82 @@
-import { useEffect } from 'react';
+import { useLayoutEffect } from 'react';
 import { useSelector } from 'react-redux';
 import { makeStyles } from '@material-ui/core';
+import clsx from 'clsx';
 
 import { MODULE_ID } from './ModuleInfo';
 
-const useStyles = makeStyles(() => ({
-  RecentVisit: {
-    '& .visited-channel-wrap': {
-      display: 'none',
+const useStyles = makeStyles(
+  () => ({
+    RecentVisit: {
+      '& .visited-channel-wrap': {
+        display: 'none',
+      },
     },
-  },
-  SideMenu: {
-    '& .right-sidebar': {
-      display: 'none',
+    SideMenu: {
+      '& .right-sidebar': {
+        display: 'none',
+      },
+      '& .board-article': {
+        margin: 0,
+      },
     },
-    '& .board-article': {
-      margin: 0,
+    Avatar: {
+      '& .avatar': {
+        display: 'none !important',
+      },
+      '& .input-wrapper > .input': {
+        width: 'calc(100% - 5rem) !important',
+      },
     },
-  },
-  Avatar: {
-    '& .avatar': {
-      display: 'none !important',
+    NotifyColor: ({ notifyColor }) => ({
+      '& .noti-menu-link span[style]': {
+        color: `${notifyColor} !important`,
+      },
+    }),
+    UserinfoWidth: ({ userinfoWidth }) => ({
+      '& .vcol.col-author': {
+        width: `calc(7rem * (1 + ${userinfoWidth * 0.01})) !important`,
+      },
+    }),
+    ResizeImage: ({ resizeImage }) => ({
+      '& .article-body img, & .article-body video:not([controls])': {
+        maxWidth: `${resizeImage}% !important`,
+      },
+    }),
+    ResizeVideo: ({ resizeVideo }) => ({
+      '& .article-body video[controls]': {
+        maxWidth: `${resizeVideo}% !important`,
+      },
+    }),
+    ModifiedIndicator: {
+      '& b.modified': {
+        display: 'none',
+      },
     },
-    '& .input-wrapper > .input': {
-      width: 'calc(100% - 5rem) !important',
+    UnfoldLongComment: {
+      '& #comment .message': {
+        maxHeight: 'none !important',
+      },
+      '& #comment .btn-more': {
+        display: 'none',
+      },
     },
-  },
-  NotifyColor: ({ notifyColor }) => ({
-    '& .noti-menu-link span[style]': {
-      color: `${notifyColor} !important`,
+    HideHumorCheckbox: {
+      '& .write-body + div': {
+        display: 'none',
+      },
     },
   }),
-  UserinfoWidth: ({ userinfoWidth }) => ({
-    '& .vcol.col-author': {
-      width: `calc(7rem * (1 + ${userinfoWidth * 0.01})) !important`,
-    },
-  }),
-  ResizeImage: ({ resizeImage }) => ({
-    '& .article-body img, & .article-body video:not([controls])': {
-      maxWidth: `${resizeImage}% !important`,
-    },
-  }),
-  ResizeVideo: ({ resizeVideo }) => ({
-    '& .article-body video[controls]': {
-      maxWidth: `${resizeVideo}% !important`,
-    },
-  }),
-  ModifiedIndicator: {
-    '& b.modified': {
-      display: 'none',
-    },
+  {
+    name: MODULE_ID,
   },
-  UnfoldLongComment: {
-    '& #comment .message': {
-      maxHeight: 'none !important',
-    },
-    '& #comment .btn-more': {
-      display: 'none',
-    },
-  },
-  HideHumorCheckbox: {
-    '& .write-body + div': {
-      display: 'none',
-    },
-  },
-}));
+);
 
 export default function LayoutCustom() {
   const config = useSelector((state) => state[MODULE_ID]);
   const classes = useStyles(config);
 
-  useEffect(() => {
+  useLayoutEffect(() => {
     const {
       recentVisit,
       sideMenu,
@@ -79,21 +85,23 @@ export default function LayoutCustom() {
       unfoldLongComment,
       hideHumorCheckbox,
     } = config;
-    const classList = [
-      ...(recentVisit ? [] : classes.RecentVisit.split(' ')),
-      ...(sideMenu ? [] : classes.SideMenu.split(' ')),
-      ...(avatar ? [] : classes.Avatar.split(' ')),
-      ...classes.NotifyColor.split(' '),
-      ...classes.UserinfoWidth.split(' '),
-      ...classes.ResizeImage.split(' '),
-      ...classes.ResizeVideo.split(' '),
-      ...(modifiedIndicator ? [] : classes.ModifiedIndicator.split(' ')),
-      ...(unfoldLongComment ? [] : classes.UnfoldLongComment.split(' ')),
-      ...(!hideHumorCheckbox ? [] : classes.HideHumorCheckbox.split(' ')),
-    ];
-    document.documentElement.classList.add(...classList);
+    const styles = clsx(
+      classes.NotifyColor,
+      classes.UserinfoWidth,
+      classes.ResizeImage,
+      classes.ResizeVideo,
+      {
+        [classes.RecentVisit]: !recentVisit,
+        [classes.SideMenu]: !sideMenu,
+        [classes.Avatar]: !avatar,
+        [classes.ModifiedIndicator]: !modifiedIndicator,
+        [classes.UnfoldLongComment]: !unfoldLongComment,
+        [classes.HideHumorCheckbox]: hideHumorCheckbox,
+      },
+    ).split(' ');
+    document.documentElement.classList.add(...styles);
 
-    return () => document.documentElement.classList.remove(...classList);
+    return () => document.documentElement.classList.remove(...styles);
   }, [classes, config]);
 
   return null;
