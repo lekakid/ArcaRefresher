7375bb2e35113fd3265ee9becde467ff3786bd21
LeKAKiD <lekakid@kakao.com>
2021-09-25 18:56:38 +0900

    feat: 섬네일 확대 구현

--- src/menu/index.jsx
+++ src/menu/index.jsx
@@ -27,6 +27,7 @@ export default () => (
   <>
     <ArticleMenu>
       <AnonymousNick.ArticleMenu />
+      <ExperienceCustom.ArticleMenu />
     </ArticleMenu>
     <ContextMenuContainer>
       <ImageDownloader.ContextMenu />
