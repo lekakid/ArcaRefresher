fae335c5931576da9dcb2af2f0dad6e3cf42a5b0
green1052 <green1052@protonmail.ch>
2021-10-03 18:58:27 +0900

    refactor: 이미지 다운로더 flag에 정규식 패턴 사용 (#12)

--- src/feature/ImageDownloader/func/replaceFlag.jsx
+++ src/feature/ImageDownloader/func/replaceFlag.jsx
@@ -11,12 +11,12 @@ export default function replaceFlag(string, flagMap) {
   } = flagMap;
 
   return string
-    .replace('%channel%', channelName)
-    .replace('%channelID%', channelID)
-    .replace('%title%', articleTitle)
-    .replace('%category%', articleCategory)
-    .replace('%author%', articleAuthor)
-    .replace('%url%', articleURL)
-    .replace('%orig%', uploadName)
-    .replace('%num%', `${index}`.padStart(3, '0'));
+    .replace(/%channel%/, channelName)
+    .replace(/%channelID%/, channelID)
+    .replace(/%title%/, articleTitle)
+    .replace(/%category%/, articleCategory)
+    .replace(/%author%/, articleAuthor)
+    .replace(/%url%/, articleURL)
+    .replace(/%orig%/, uploadName)
+    .replace(/%num%/, `${index}`.padStart(3, '0'));
 }
