ef0d4fd6b9acf6ffdd250a7ea026e1cd81955478
lekakid <lekakid@kakao.com>
2021-10-30 22:33:52 +0900

    fix: 마우스 제스쳐 관련 확장 충돌 방지 (#43)

--- src/menu/ContextMenu/useContextMenu.jsx
+++ src/menu/ContextMenu/useContextMenu.jsx
@@ -1,4 +1,4 @@
-import { useEffect, useState } from 'react';
+import { useEffect, useRef, useState } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
 import { setOpen } from './slice';
 import { MODULE_ID } from './ModuleInfo';
@@ -9,12 +9,27 @@ export default function useContextMenu({ trigger, dataGetter }) {
     config: { enabled },
   } = useSelector((state) => state[MODULE_ID]);
   const [data, setData] = useState(null);
+  const mouseInfo = useRef({ right: false, count: 0 });
 
   useEffect(() => {
     if (!enabled) return null;
 
+    const handleDown = ({ button }) => {
+      if (button === 2) mouseInfo.current.right = true;
+    };
+    const handleUp = ({ button }) => {
+      if (button === 2) mouseInfo.current.right = false;
+    };
+    const handleMove = () => {
+      if (mouseInfo.current.right) mouseInfo.current.count += 1;
+    };
+
     const handleContext = (e) => {
       if (e.shiftKey) return;
+      if (mouseInfo.current.count > 20) {
+        mouseInfo.current.count = 0;
+        return;
+      }
 
       if (!trigger(e)) {
         setData(null);
@@ -26,8 +41,16 @@ export default function useContextMenu({ trigger, dataGetter }) {
       dispatch(setOpen());
     };
 
+    document.addEventListener('mousedown', handleDown);
+    document.addEventListener('mouseup', handleUp);
+    document.addEventListener('mousemove', handleMove);
     document.addEventListener('contextmenu', handleContext);
-    return () => document.removeEventListener('contextmenu', handleContext);
+    return () => {
+      document.removeEventListener('mousedown', handleDown);
+      document.removeEventListener('mouseup', handleUp);
+      document.removeEventListener('mousemove', handleMove);
+      document.removeEventListener('contextmenu', handleContext);
+    };
   }, [dataGetter, dispatch, enabled, trigger]);
 
   return data;
