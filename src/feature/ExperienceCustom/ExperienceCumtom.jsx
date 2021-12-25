e81cfb98456abb03d7e7ca33239659850f72f5fd
LeKAKiD <lekakid@kakao.com>
2021-12-25 16:12:41 +0900

    [사용자 경험] 공식 기능 지원에 따른 댓글 바로가기 기능 삭제

--- src/feature/ExperienceCustom/ExperienceCumtom.jsx
+++ src/feature/ExperienceCustom/ExperienceCumtom.jsx
@@ -64,23 +64,6 @@ export default function ExperienceCustomizer() {
   }, [articleLoaded]);
 
   useEffect(() => {
-    if (!article) return;
-
-    const replyCount = document.querySelector(
-      '.article-info > span:nth-child(7)',
-    );
-    if (!replyCount) return;
-
-    replyCount.addEventListener('click', () => {
-      const commentForm = document.querySelector(COMMENT_VIEW);
-      window.scrollTo({
-        top: commentForm.offsetTop - window.innerHeight * 0.3,
-        behavior: 'smooth',
-      });
-    });
-  }, [article]);
-
-  useEffect(() => {
     if (!article || !blockMediaNewWindow) return;
 
     article.querySelectorAll(ARTICLE_IMAGES).forEach((i) => {
