33f3dd7c066e8c29f306b7fc16ad7b6152bd23be
LeKAKiD <lekakid@kakao.com>
2022-04-22 17:44:19 +0900

    [게시물 메뉴] 레이블 추가

--- src/menu/ArticleMenu/ArticleMenu.jsx
+++ src/menu/ArticleMenu/ArticleMenu.jsx
@@ -1,11 +1,10 @@
 import React, { useEffect, useState } from 'react';
-import ReactDOM from 'react-dom';
-import { makeStyles } from '@material-ui/core';
+import { Grid, makeStyles, Portal, Typography } from '@material-ui/core';
 
 import { ARTICLE_HEADER_MENU, ARTICLE_LOADED } from 'core/selector';
 import { useElementQuery } from 'core/hooks';
 
-const useStyles = makeStyles(() => ({
+const useStyles = makeStyles((theme) => ({
   '@global': {
     '.edit-menu:empty': {
       display: 'none',
@@ -13,13 +12,16 @@ const useStyles = makeStyles(() => ({
   },
   root: {
     borderTop: '1px solid var(--color-border-outer)',
-    textAlign: 'end',
-    '&:empty': {
-      display: 'none',
-    },
     '& .MuiButton-root': {
       color: 'var(--color-text-color)',
     },
+  },
+  label: {
+    paddingLeft: theme.spacing(1),
+  },
+  buttons: {
+    paddingRight: theme.spacing(1),
+    textAlign: 'end',
   },
 }));
 
@@ -42,8 +44,16 @@ export default function ArticleMenu({ children }) {
   const classes = useStyles();
 
   if (!container) return null;
-  return ReactDOM.createPortal(
-    <div className={classes.root}>{children}</div>,
-    container,
+  return (
+    <Portal container={container}>
+      <Grid container alignItems="center" classes={{ root: classes.root }}>
+        <Grid item xs={12} sm={3} classes={{ root: classes.label }}>
+          <Typography variant="subtitle1">리프레셔 메뉴</Typography>
+        </Grid>
+        <Grid item xs={12} sm={9} classes={{ root: classes.buttons }}>
+          {children}
+        </Grid>
+      </Grid>
+    </Portal>
   );
 }
