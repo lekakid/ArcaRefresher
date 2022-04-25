4631124fccad17354c7a52dbb3cf8aa3e879ff33
LeKAKiD <lekakid@kakao.com>
2022-04-25 20:48:39 +0900

    [댓글 새로고침] 사이트 구조 변경 대응

--- src/feature/CommentRefresh/CommentRefresh.jsx
+++ src/feature/CommentRefresh/CommentRefresh.jsx
@@ -1,4 +1,4 @@
-import React, { useCallback, useEffect, useState } from 'react';
+import React, { useCallback, useEffect, useRef, useState } from 'react';
 import { IconButton, Portal } from '@material-ui/core';
 import { withStyles } from '@material-ui/styles';
 import { Refresh } from '@material-ui/icons';
@@ -8,12 +8,11 @@ import {
   COMMENT_LOADED,
   COMMENT_TITLE,
   COMMENT_SUBTITLE,
+  COMMENT_INNER_VIEW,
 } from 'core/selector';
 import { useElementQuery } from 'core/hooks';
 import { dispatchAREvent, EVENT_COMMENT_REFRESH } from 'core/event';
 import { getDateStr } from 'util/time';
-
-import getNewComment from './getNewComment';
 
 const style = {
   '@global': {
@@ -37,62 +36,58 @@ function CommentRefresh({ classes }) {
     top: undefined,
     bottom: undefined,
   });
-  const [comment, setComment] = useState(undefined);
+  const comment = useRef(undefined);
   const commentLoaded = useElementQuery(COMMENT_LOADED);
 
   // 초기화
   useEffect(() => {
-    if (commentLoaded) {
-      const commentView = document.querySelector(COMMENT_VIEW);
-      setComment(commentView);
-      const top = document.createElement('span');
-      const bottom = document.createElement('span');
-      document.querySelector(COMMENT_TITLE)?.append(top);
-      document.querySelector(COMMENT_SUBTITLE)?.prepend(bottom);
+    if (!commentLoaded) return;
 
-      setTitle({ top, bottom });
-    }
+    const initComment = document.querySelector(COMMENT_INNER_VIEW);
+    comment.current = initComment;
+    const top = document.createElement('span');
+    const bottom = document.createElement('span');
+    document.querySelector(COMMENT_TITLE)?.append(top);
+    document.querySelector(COMMENT_SUBTITLE)?.prepend(bottom);
+
+    setTitle({ top, bottom });
   }, [commentLoaded]);
 
   useEffect(() => {
-    if (!comment) return undefined;
+    if (!commentLoaded) return;
 
     const observer = new MutationObserver(() => {
-      if (!comment.parentNode) {
-        const commentView = document.querySelector(COMMENT_VIEW);
-        setComment(commentView);
+      if (comment.current.parentElement) return;
 
-        document.querySelector(COMMENT_TITLE).append(title.top);
-        document.querySelector(COMMENT_SUBTITLE).prepend(title.bottom);
-        dispatchAREvent(EVENT_COMMENT_REFRESH);
-      }
+      comment.current = document.querySelector(COMMENT_INNER_VIEW);
+      dispatchAREvent(EVENT_COMMENT_REFRESH);
     });
-    observer.observe(comment.parentNode, {
+    observer.observe(comment.current.closest(COMMENT_VIEW), {
       childList: true,
       subtree: true,
     });
-    return () => {
-      observer.disconnect();
-    };
-  }, [comment, title]);
+  }, [commentLoaded]);
 
   const handleClick = useCallback(async () => {
-    if (!comment) return;
-
-    const newComments = await getNewComment();
-    if (newComments) {
-      comment.replaceWith(newComments);
-      // 입력창이 존재한다면 가져오기 (기존 이벤트들 유지)
-      const input = comment.querySelector('form.reply-form');
-      if (input) {
-        newComments.querySelector('form.reply-form').replaceWith(input);
-      }
-      newComments.querySelectorAll('time').forEach((time) => {
-        // eslint-disable-next-line no-param-reassign
-        time.textContent = getDateStr(time.dateTime, 'year-month-day hh:mm:ss');
-      });
+    const response = await fetch(window.location.href);
+    if (!response.ok) {
+      console.warn('[CommentRefresh] 네트워크 오류');
+      return;
     }
-  }, [comment]);
+
+    const text = await response.text();
+    const parser = new DOMParser();
+    const resultDocument = parser.parseFromString(text, 'text/html');
+    const newComments = resultDocument.querySelector(COMMENT_INNER_VIEW);
+    if (!newComments) return;
+
+    newComments.querySelectorAll('time').forEach((time) => {
+      // eslint-disable-next-line no-param-reassign
+      time.textContent = getDateStr(time.dateTime, 'year-month-day hh:mm:ss');
+    });
+
+    comment.current.replaceWith(newComments);
+  }, []);
 
   return (
     <>
