1ac205300cef00833667d791ce0804bf08ce19a9
LeKAKiD <lekakid@kakao.com>
2022-09-29 00:39:06 +0900

    [func/url] regex 대체 코드 사용

--- src/func/url.jsx
+++ src/func/url.jsx
@@ -1,9 +1,6 @@
 export function getArcaMediaURL(url, type) {
-  const typeQuery = type ? `?type=${type}` : '';
-
-  const regex = /ac[0-9]*\.namu\.la/;
-  if (regex.test(url)) {
-    return `${url.replace('.namu.la', '-p.namu.la')}${typeQuery}`;
-  }
-  return `${url}${typeQuery}`;
+  const urlQuery = type ? `?type=${type}` : '?';
+  const originServer =
+    url.indexOf('-p') < 0 ? url.replace('.namu.la', '-p.namu.la') : url;
+  return `${originServer}${urlQuery}`;
 }
