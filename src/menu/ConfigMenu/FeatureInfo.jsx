add200f622fad10c34818abdcd868464288e0d05
LeKAKiD <lekakid@kakao.com>
2022-10-19 15:50:11 +0900

    [ConfigMenu] FeatureInfo 변경 누락 수정

---
+++ src/menu/ConfigMenu/FeatureInfo.jsx
@@ -0,0 +1,8 @@
+import 'core/types';
+
+/** @type {FeatureInfo} */
+export default {
+  ID: 'Config',
+  name: '설정 모듈',
+  description: '전체 모듈의 설정을 관리하는 메뉴',
+};
--- src/menu/ConfigMenu/ModuleInfo.jsx
+++
@@ -1,3 +0,0 @@
-export const MODULE_ID = 'Config';
-export const MODULE_NAME = '설정 모듈';
-export const MODULE_DESCRIPTION = `전체 모듈의 설정을 관리하는 메뉴를 제공합니다.`;
