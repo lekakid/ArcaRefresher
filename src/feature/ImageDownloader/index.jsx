e58046a61b22240c7a17e13b502884b7a9d5417d
LeKAKiD <lekakid@kakao.com>
2022-10-19 22:27:37 +0900

    [ImageDownloader] 테스트 코드 커밋 롤백

--- src/feature/ImageDownloader/index.jsx
+++ src/feature/ImageDownloader/index.jsx
@@ -1,6 +1,5 @@
 import ConfigMenu from './ConfigMenu';
-import ArticleMenu from './ArticleMenu';
 import ContextMenu from './ContextMenu';
 import Feature from './ImageDownloader';
 
-export { ConfigMenu, ArticleMenu, ContextMenu, Feature };
+export { ConfigMenu, ContextMenu, Feature };
