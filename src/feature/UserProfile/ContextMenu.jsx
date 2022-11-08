225eaff3fdbb1d7d1de9b503075fbebec30bbe73
LeKAKiD <lekakid@kakao.com>
2022-11-08 23:44:41 +0900

    [UserProfile] 멘션 아이디 복사 기능 추가

--- src/feature/UserProfile/ContextMenu.jsx
+++ src/feature/UserProfile/ContextMenu.jsx
@@ -1,13 +1,13 @@
 import React, { useCallback, useEffect, useState } from 'react';
 import { List, ListItemIcon, MenuItem, Typography } from '@material-ui/core';
-import { Person } from '@material-ui/icons';
+import { Assignment, Person } from '@material-ui/icons';
 
 import { USER_INFO } from 'core/selector';
 import { useContextMenu } from 'menu/ContextMenu';
-import { getUserInfo } from 'func/user';
+import { getUserNick } from 'func/user';
 
 function ContextMenu({ targetRef }) {
-  const [open, closeMenu] = useContextMenu({
+  const [open, closeMenu, setSnack] = useContextMenu({
     method: 'closest',
     selector: USER_INFO,
   });
@@ -22,29 +22,41 @@ function ContextMenu({ targetRef }) {
     const userInfo = targetRef.current.closest(USER_INFO);
     if (!userInfo) return;
 
-    const id = getUserInfo(userInfo);
+    const id = getUserNick(userInfo);
     if (id.includes('.')) {
       // 유동
       setData(undefined);
       return;
     }
 
-    setData(id.replace('#', '/'));
+    setData({ id, url: id.replace('#', '/') });
   }, [open, targetRef]);
 
-  const handleInfo = useCallback(() => {
-    window.open(`https://arca.live/u/@${data}`);
+  const handleProfile = useCallback(() => {
+    window.open(`https://arca.live/u/@${data.url}`);
     closeMenu();
   }, [closeMenu, data]);
+
+  const handleCopyId = useCallback(async () => {
+    closeMenu();
+    await navigator.clipboard.writeText(`@${data.id}`);
+    setSnack({ msg: '아이디가 복사되었습니다.', time: 3000 });
+  }, [closeMenu, data, setSnack]);
 
   if (!data) return null;
   return (
     <List>
-      <MenuItem onClick={handleInfo}>
+      <MenuItem onClick={handleProfile}>
         <ListItemIcon>
           <Person />
         </ListItemIcon>
         <Typography>사용자 정보</Typography>
+      </MenuItem>
+      <MenuItem onClick={handleCopyId}>
+        <ListItemIcon>
+          <Assignment />
+        </ListItemIcon>
+        <Typography>멘션 아이디 복사</Typography>
       </MenuItem>
     </List>
   );
