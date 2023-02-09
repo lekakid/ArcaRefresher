ded8ac7d9e2cc43aa2b8f0d9161f4a0261681b0f
LeKAKiD <lekakid@kakao.com>
2023-02-09 17:01:08 +0900

    [func/url/getArcaMedia] CORS 문제 수정

--- src/func/url.jsx
+++ src/func/url.jsx
@@ -1,6 +1,8 @@
 export function getArcaMediaURL(url, type) {
-  const urlQuery = type && `?type=${type}`;
-  const originServer =
-    url.includes('-p') ? url : url.replace('.namu.la', '-p.namu.la');
+  // 아카 이미지 서버 CORS 대응, '?' 없애지 마세요
+  const urlQuery = type ? `?type=${type}` : '?';
+  const originServer = url.includes('-p')
+    ? url
+    : url.replace('.namu.la', '-p.namu.la');
   return `${originServer}${urlQuery}`;
 }
