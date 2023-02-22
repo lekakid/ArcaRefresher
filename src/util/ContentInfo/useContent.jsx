3ac2d26c3ae316e7d27afc24146c73c22835679d
LeKAKiD <lekakid@kakao.com>
2023-02-22 14:21:40 +0900

    [ContentInfo] useContent 주석 추가

--- src/util/ContentInfo/useContent.jsx
+++ src/util/ContentInfo/useContent.jsx
@@ -1,6 +1,23 @@
 import { useSelector } from 'react-redux';
 import Info from './FeatureInfo';
 
+/**
+ * 게시판 및 게시물 정보를 받아옵니다.
+ * @returns {{
+ *  channel: {
+ *    ID: string,
+ *    name: string,
+ *    category: [{id, text}]
+ *  },
+ *  article: {
+ *    ID: string,
+ *    category: string,
+ *    title: string,
+ *    author: string,
+ *    url: string
+ *  }
+ * }}
+ */
 export default function useContent() {
   return useSelector((state) => state[Info.ID]);
 }
