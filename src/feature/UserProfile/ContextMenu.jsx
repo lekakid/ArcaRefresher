22d2e8f6b260784903fe4472c4e3671730d9b99a
LeKAKiD <lekakid@kakao.com>
2023-02-22 14:28:32 +0900

    [UserProfile] 이용자 검색 바로가기 추가

--- src/feature/UserProfile/ContextMenu.jsx
+++ src/feature/UserProfile/ContextMenu.jsx
@@ -1,13 +1,15 @@
 import React, { useCallback } from 'react';
 import { List, ListItemIcon, MenuItem, Typography } from '@material-ui/core';
-import { Assignment, Person } from '@material-ui/icons';
+import { Assignment, Person, Search } from '@material-ui/icons';
 
 import { USER_INFO } from 'core/selector';
 import { useContextMenu, useContextSnack } from 'menu/ContextMenu';
 import { getUserNick } from 'func/user';
+import { useContent } from 'util/ContentInfo';
 
 function ContextMenu({ targetRef }) {
   const setSnack = useContextSnack();
+  const { channel } = useContent();
   const [data, closeMenu] = useContextMenu({
     targetRef,
     selector: USER_INFO,
@@ -30,6 +32,24 @@ function ContextMenu({ targetRef }) {
     setSnack({ msg: '아이디가 복사되었습니다.', time: 3000 });
   }, [closeMenu, data, setSnack]);
 
+  const handleSearchAll = useCallback(async () => {
+    window.open(
+      `https://arca.live/b/breaking?target=nickname&keyword=${
+        data.id.split('#')[0]
+      }`,
+    );
+    closeMenu();
+  }, [closeMenu, data]);
+
+  const handleSearchChannel = useCallback(async () => {
+    window.open(
+      `https://arca.live/b/${channel.ID}?target=nickname&keyword=${
+        data.id.split('#')[0]
+      }`,
+    );
+    closeMenu();
+  }, [channel, closeMenu, data]);
+
   if (!data) return null;
   return (
     <List>
@@ -44,6 +64,18 @@ function ContextMenu({ targetRef }) {
           <Assignment />
         </ListItemIcon>
         <Typography>멘션 아이디 복사</Typography>
+      </MenuItem>
+      <MenuItem onClick={handleSearchChannel}>
+        <ListItemIcon>
+          <Search />
+        </ListItemIcon>
+        <Typography>채널 내 검색</Typography>
+      </MenuItem>
+      <MenuItem onClick={handleSearchAll}>
+        <ListItemIcon>
+          <Search />
+        </ListItemIcon>
+        <Typography>종합속보 검색</Typography>
       </MenuItem>
     </List>
   );
