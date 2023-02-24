cbaa00aad3c943cde6b3aa838aa13840845c0355
LeKAKiD <lekakid@kakao.com>
2023-02-24 21:27:31 +0900

    [component] GroupableSelect 공용 컴포넌트로 분리

--- src/component/config/GroupableSelect.jsx
+++ src/component/config/GroupableSelect.jsx
@@ -2,11 +2,11 @@ import { Select } from '@material-ui/core';
 import React from 'react';
 
 /**
- * EditPresetSelector
- * Material UI Selector와 같습니다.
- * ButtonGroup에 넣어서 사용할 때 ButtonProps 들을 제외해 디버그 메세지를 정리합니다.
+ * GroupableSelect
+ * ButtonGroup에 넣어서 사용할 수 있습니다.
+ * 존재하지 않는 props 관련 디버그 메세지를 정리합니다.
  */
-export default function EditPresetSelector({
+export default function GroupableSelect({
   color,
   disableElevation,
   disableFocusRipple,
