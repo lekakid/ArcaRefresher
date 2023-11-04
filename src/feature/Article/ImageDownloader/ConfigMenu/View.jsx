8ba082395b6f0c9ccfe3e0609ebbd1f07b56c783
LeKAKiD <lekakid@kakao.com>
2023-11-04 21:09:35 +0900

    [ImageDownloader] 포맷 텍스트 필드 구조 개선

--- src/feature/Article/ImageDownloader/ConfigMenu/View.jsx
+++ src/feature/Article/ImageDownloader/ConfigMenu/View.jsx
@@ -1,16 +1,6 @@
-import React, { useCallback } from 'react';
-import { useDispatch, useSelector } from 'react-redux';
-import {
-  List,
-  ListItem,
-  ListItemText,
-  ListItemSecondaryAction,
-  Paper,
-  TextField,
-  Typography,
-  Box,
-  MenuItem,
-} from '@material-ui/core';
+import React from 'react';
+import { useSelector } from 'react-redux';
+import { List, Paper, Typography, Box, MenuItem } from '@material-ui/core';
 
 import { SelectRow, SwitchRow } from 'component/config';
 
@@ -24,10 +14,9 @@ import {
   $setZipExtension,
 } from '../slice';
 import { FORMAT_STRING, LABEL } from '../func/format';
-import FormatSelector from './FormatSelector';
+import FormatTextFieldRow from './FormatTextFieldRow';
 
 const View = React.forwardRef((_props, ref) => {
-  const dispatch = useDispatch();
   const {
     enabled,
     downloadMethod,
@@ -36,47 +25,6 @@ const View = React.forwardRef((_props, ref) => {
     zipExtension,
     zipImageName,
   } = useSelector((state) => state[Info.ID].storage);
-
-  const handleFileName = useCallback(
-    (e) => {
-      dispatch($setFileName(e.target.value));
-    },
-    [dispatch],
-  );
-
-  const handleAddFormatFileName = useCallback(
-    (value) => {
-      dispatch($setFileName(`${fileName}${value}`));
-    },
-    [dispatch, fileName],
-  );
-
-  const handleZipName = useCallback(
-    (e) => {
-      dispatch($setZipName(e.target.value));
-    },
-    [dispatch],
-  );
-  const handleAddFormatZipName = useCallback(
-    (value) => {
-      dispatch($setZipName(`${zipName}${value}`));
-    },
-    [dispatch, zipName],
-  );
-
-  const handleZipImageName = useCallback(
-    (e) => {
-      dispatch($setZipImageName(e.target.value));
-    },
-    [dispatch],
-  );
-
-  const handleAddFormatZipImageName = useCallback(
-    (value) => {
-      dispatch($setZipImageName(`${zipImageName}${value}`));
-    },
-    [dispatch, zipImageName],
-  );
 
   return (
     <Box ref={ref}>
@@ -98,50 +46,45 @@ const View = React.forwardRef((_props, ref) => {
             <MenuItem value="fetch">fetch</MenuItem>
             <MenuItem value="xhr">XHR</MenuItem>
           </SelectRow>
-          <ListItem>
-            <ListItemText primary="우클릭 저장 시 이미지 이름" />
-          </ListItem>
-          <ListItem divider>
-            <TextField fullWidth value={fileName} onChange={handleFileName} />
-            <ListItemSecondaryAction>
-              <FormatSelector
-                formatList={[
-                  { value: FORMAT_STRING.CHANNEL, label: LABEL.CHANNEL },
-                  { value: FORMAT_STRING.CHANNEL_ID, label: LABEL.CHANNEL_ID },
-                  { value: FORMAT_STRING.TITLE, label: LABEL.TITLE },
-                  { value: FORMAT_STRING.CATEGORY, label: LABEL.CATEGORY },
-                  { value: FORMAT_STRING.AUTHOR, label: LABEL.AUTHOR },
-                  { value: FORMAT_STRING.ARTICLE_ID, label: LABEL.ARTICLE_ID },
-                  { value: FORMAT_STRING.URL, label: LABEL.URL },
-                  {
-                    value: FORMAT_STRING.UPLOAD_NAME,
-                    label: LABEL.UPLOAD_NAME,
-                  },
-                ]}
-                onSelect={handleAddFormatFileName}
-              />
-            </ListItemSecondaryAction>
-          </ListItem>
-          <ListItem>
-            <ListItemText primary="일괄 다운로드 시 압축파일 이름" />
-          </ListItem>
-          <ListItem divider>
-            <TextField fullWidth value={zipName} onChange={handleZipName} />
-            <ListItemSecondaryAction>
-              <FormatSelector
-                formatList={[
-                  { value: FORMAT_STRING.CHANNEL, label: LABEL.CHANNEL },
-                  { value: FORMAT_STRING.CHANNEL_ID, label: LABEL.CHANNEL_ID },
-                  { value: FORMAT_STRING.TITLE, label: LABEL.TITLE },
-                  { value: FORMAT_STRING.CATEGORY, label: LABEL.CATEGORY },
-                  { value: FORMAT_STRING.AUTHOR, label: LABEL.AUTHOR },
-                  { value: FORMAT_STRING.ARTICLE_ID, label: LABEL.ARTICLE_ID },
-                  { value: FORMAT_STRING.URL, label: LABEL.URL },
-                ]}
-                onSelect={handleAddFormatZipName}
-              />
-            </ListItemSecondaryAction>
-          </ListItem>
+          <FormatTextFieldRow
+            divider
+            primary="우클릭 저장 시 이미지 이름"
+            value={fileName}
+            action={$setFileName}
+          >
+            <MenuItem value={FORMAT_STRING.CHANNEL}>{LABEL.CHANNEL}</MenuItem>
+            <MenuItem value={FORMAT_STRING.CHANNEL_ID}>
+              {LABEL.CHANNEL_ID}
+            </MenuItem>
+            <MenuItem value={FORMAT_STRING.TITLE}>{LABEL.TITLE}</MenuItem>
+            <MenuItem value={FORMAT_STRING.CATEGORY}>{LABEL.CATEGORY}</MenuItem>
+            <MenuItem value={FORMAT_STRING.AUTHOR}>{LABEL.AUTHOR}</MenuItem>
+            <MenuItem value={FORMAT_STRING.ARTICLE_ID}>
+              {LABEL.ARTICLE_ID}
+            </MenuItem>
+            <MenuItem value={FORMAT_STRING.URL}>{LABEL.URL}</MenuItem>
+            <MenuItem value={FORMAT_STRING.UPLOAD_NAME}>
+              {LABEL.UPLOAD_NAME}
+            </MenuItem>
+          </FormatTextFieldRow>
+          <FormatTextFieldRow
+            divider
+            primary="일괄 다운로드 시 압축파일 이름"
+            value={zipName}
+            action={$setZipName}
+          >
+            <MenuItem value={FORMAT_STRING.CHANNEL}>{LABEL.CHANNEL}</MenuItem>
+            <MenuItem value={FORMAT_STRING.CHANNEL_ID}>
+              {LABEL.CHANNEL_ID}
+            </MenuItem>
+            <MenuItem value={FORMAT_STRING.TITLE}>{LABEL.TITLE}</MenuItem>
+            <MenuItem value={FORMAT_STRING.CATEGORY}>{LABEL.CATEGORY}</MenuItem>
+            <MenuItem value={FORMAT_STRING.AUTHOR}>{LABEL.AUTHOR}</MenuItem>
+            <MenuItem value={FORMAT_STRING.ARTICLE_ID}>
+              {LABEL.ARTICLE_ID}
+            </MenuItem>
+            <MenuItem value={FORMAT_STRING.URL}>{LABEL.URL}</MenuItem>
+          </FormatTextFieldRow>
           <SelectRow
             divider
             primary="일괄 다운로드 시 압축파일 확장자"
@@ -151,35 +94,27 @@ const View = React.forwardRef((_props, ref) => {
             <MenuItem value="zip">zip</MenuItem>
             <MenuItem value="cbz">cbz</MenuItem>
           </SelectRow>
-          <ListItem>
-            <ListItemText primary="일괄 다운로드 시 압축파일 내 이미지 이름" />
-          </ListItem>
-          <ListItem>
-            <TextField
-              fullWidth
-              value={zipImageName}
-              onChange={handleZipImageName}
-            />
-            <ListItemSecondaryAction>
-              <FormatSelector
-                formatList={[
-                  { value: FORMAT_STRING.CHANNEL, label: LABEL.CHANNEL },
-                  { value: FORMAT_STRING.CHANNEL_ID, label: LABEL.CHANNEL_ID },
-                  { value: FORMAT_STRING.TITLE, label: LABEL.TITLE },
-                  { value: FORMAT_STRING.CATEGORY, label: LABEL.CATEGORY },
-                  { value: FORMAT_STRING.AUTHOR, label: LABEL.AUTHOR },
-                  { value: FORMAT_STRING.ARTICLE_ID, label: LABEL.ARTICLE_ID },
-                  { value: FORMAT_STRING.URL, label: LABEL.URL },
-                  {
-                    value: FORMAT_STRING.UPLOAD_NAME,
-                    label: LABEL.UPLOAD_NAME,
-                  },
-                  { value: FORMAT_STRING.NUMBER, label: LABEL.NUMBER },
-                ]}
-                onSelect={handleAddFormatZipImageName}
-              />
-            </ListItemSecondaryAction>
-          </ListItem>
+          <FormatTextFieldRow
+            primary="일괄 다운로드 시 압축파일 내 이미지 이름"
+            value={zipImageName}
+            action={$setZipImageName}
+          >
+            <MenuItem value={FORMAT_STRING.CHANNEL}>{LABEL.CHANNEL}</MenuItem>
+            <MenuItem value={FORMAT_STRING.CHANNEL_ID}>
+              {LABEL.CHANNEL_ID}
+            </MenuItem>
+            <MenuItem value={FORMAT_STRING.TITLE}>{LABEL.TITLE}</MenuItem>
+            <MenuItem value={FORMAT_STRING.CATEGORY}>{LABEL.CATEGORY}</MenuItem>
+            <MenuItem value={FORMAT_STRING.AUTHOR}>{LABEL.AUTHOR}</MenuItem>
+            <MenuItem value={FORMAT_STRING.ARTICLE_ID}>
+              {LABEL.ARTICLE_ID}
+            </MenuItem>
+            <MenuItem value={FORMAT_STRING.URL}>{LABEL.URL}</MenuItem>
+            <MenuItem value={FORMAT_STRING.UPLOAD_NAME}>
+              {LABEL.UPLOAD_NAME}
+            </MenuItem>
+            <MenuItem value={FORMAT_STRING.NUMBER}>{LABEL.NUMBER}</MenuItem>
+          </FormatTextFieldRow>
         </List>
       </Paper>
     </Box>
