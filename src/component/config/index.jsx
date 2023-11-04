c10b60a65efd2065c440d68d7b47d24ddc435ead
LeKAKiD <lekakid@kakao.com>
2023-11-04 21:57:31 +0900

    [component] config/SliderRow 추가

--- src/component/config/index.jsx
+++ src/component/config/index.jsx
@@ -1,6 +1,7 @@
 import SelectRow from './SelectRow';
+import SliderRow from './SliderRow';
 import SwitchRow from './SwitchRow';
 import TextEditorRow from './TextEditorRow';
 import TableEditorRow from './TableEditorRow';
 
-export { SelectRow, SwitchRow, TextEditorRow, TableEditorRow };
+export { SelectRow, SliderRow, SwitchRow, TextEditorRow, TableEditorRow };
--- src/feature/Site/LayoutCustom/ConfigMenu/View.jsx
+++ src/feature/Site/LayoutCustom/ConfigMenu/View.jsx
@@ -1,25 +1,19 @@
-import React, { useCallback } from 'react';
-import { useDispatch, useSelector } from 'react-redux';
+import React from 'react';
+import { useSelector } from 'react-redux';
 import {
   Box,
   Collapse,
   List,
-  ListItem,
-  ListItemSecondaryAction,
-  ListItemText,
   MenuItem,
   Paper,
-  Slider,
   Typography,
   useMediaQuery,
 } from '@material-ui/core';
 
-import { useOpacity } from 'menu/ConfigMenu';
-import { SelectRow, SwitchRow } from 'component/config';
+import { SelectRow, SwitchRow, SliderRow } from 'component/config';
 import Info from '../FeatureInfo';
 import {
   $toggleEnable,
-  $setFontSize,
   $setNotifyPosition,
   $toggleTopNews,
   $setRecentVisit,
@@ -27,9 +21,6 @@ import {
   $toggleSideMenu,
   $toggleAvatar,
   $setUserInfoWith,
-  $setResizeImage,
-  $setResizeVideo,
-  $setResizeEmoticonPalette,
   $toggleUnvote,
   $toggleModifiedIndicator,
   $toggleLongComment,
@@ -38,6 +29,10 @@ import {
   $toggleSideBests,
   $toggleDarkModeWriteForm,
   $toggleReverseComment,
+  $setResizeImage,
+  $setResizeVideo,
+  $setResizeEmoticonPalette,
+  $setFontSize,
 } from '../slice';
 
 function labelFormat(x) {
@@ -49,7 +44,6 @@ function emotLabelFormat(x) {
 }
 
 const View = React.forwardRef((_props, ref) => {
-  const dispatch = useDispatch();
   const {
     enabled,
     fontSize,
@@ -73,52 +67,6 @@ const View = React.forwardRef((_props, ref) => {
     fixDarkModeWriteForm,
   } = useSelector((state) => state[Info.ID].storage);
   const mobile = useMediaQuery((theme) => theme.breakpoints.down('sm'));
-  const setOpacity = useOpacity();
-
-  const SliderCommited = () => {
-    setOpacity(1);
-  };
-
-  // ------ 사이트 ------
-  const handleUserinfoWidth = useCallback(
-    (e, value) => {
-      setOpacity(0.6);
-      dispatch($setUserInfoWith(value));
-    },
-    [dispatch, setOpacity],
-  );
-
-  // ------ 게시물 ------
-  const handleResizeImage = useCallback(
-    (e, value) => {
-      setOpacity(0.6);
-      dispatch($setResizeImage(value));
-    },
-    [dispatch, setOpacity],
-  );
-
-  const handleResizeVideo = useCallback(
-    (e, value) => {
-      setOpacity(0.6);
-      dispatch($setResizeVideo(value));
-    },
-    [dispatch, setOpacity],
-  );
-
-  const handleEmoticonPalette = useCallback(
-    (e, value) => {
-      dispatch($setResizeEmoticonPalette(value));
-    },
-    [dispatch],
-  );
-
-  // ------ 접근성 ------
-  const handleFontSize = useCallback(
-    (_e, value) => {
-      dispatch($setFontSize(value));
-    },
-    [dispatch],
-  );
 
   return (
     <Box ref={ref}>
@@ -201,41 +149,31 @@ const View = React.forwardRef((_props, ref) => {
             value={avatar}
             action={$toggleAvatar}
           />
-          <ListItem>
-            <ListItemText>게시판 이용자 너비</ListItemText>
-            <ListItemSecondaryAction>
-              <Slider
-                value={userinfoWidth}
-                onChange={handleUserinfoWidth}
-                onChangeCommitted={SliderCommited}
-              />
-            </ListItemSecondaryAction>
-          </ListItem>
+          <SliderRow
+            primary="게시판 이용자 너비"
+            value={userinfoWidth}
+            action={$setUserInfoWith}
+            opacityOnChange={0.6}
+          />
         </List>
       </Paper>
       <Typography variant="subtitle2">게시물</Typography>
       <Paper>
         <List disablePadding>
-          <ListItem divider>
-            <ListItemText>이미지 크기</ListItemText>
-            <ListItemSecondaryAction>
-              <Slider
-                value={resizeImage}
-                onChange={handleResizeImage}
-                onChangeCommitted={SliderCommited}
-              />
-            </ListItemSecondaryAction>
-          </ListItem>
-          <ListItem divider>
-            <ListItemText>동영상 크기</ListItemText>
-            <ListItemSecondaryAction>
-              <Slider
-                value={resizeVideo}
-                onChange={handleResizeVideo}
-                onChangeCommitted={SliderCommited}
-              />
-            </ListItemSecondaryAction>
-          </ListItem>
+          <SliderRow
+            divider
+            primary="이미지 크기"
+            value={resizeImage}
+            action={$setResizeImage}
+            opacityOnChange={0.6}
+          />
+          <SliderRow
+            divider
+            primary="동영상 크기"
+            value={resizeVideo}
+            action={$setResizeVideo}
+            opacityOnChange={0.6}
+          />
           <SwitchRow
             primary="비추천 버튼 숨김"
             value={hideUnvote}
@@ -271,42 +209,34 @@ const View = React.forwardRef((_props, ref) => {
             value={hideVoiceComment}
             action={$toggleHideVoiceComment}
           />
-          <ListItem>
-            <ListItemText>이모티콘 선택창 높이</ListItemText>
-            <ListItemSecondaryAction>
-              <Slider
-                value={resizeEmoticonPalette}
-                min={2}
-                max={5}
-                step={1}
-                marks
-                valueLabelFormat={emotLabelFormat}
-                valueLabelDisplay="auto"
-                onChange={handleEmoticonPalette}
-              />
-            </ListItemSecondaryAction>
-          </ListItem>
+          <SliderRow
+            primary="이모티콘 선택창 높이"
+            min={2}
+            max={5}
+            step={1}
+            marks
+            valueLabelFormat={emotLabelFormat}
+            valueLabelDisplay="auto"
+            value={resizeEmoticonPalette}
+            action={$setResizeEmoticonPalette}
+          />
         </List>
       </Paper>
       <Typography variant="subtitle2">접근성</Typography>
       <Paper>
         <List disablePadding>
-          <ListItem divider>
-            <ListItemText
-              primary="사이트 전체 폰트 크기"
-              secondary="표시 설정에서 글자 크기 브라우저 기본 설정 필요"
-            />
-            <ListItemSecondaryAction>
-              <Slider
-                min={8}
-                max={30}
-                valueLabelDisplay="auto"
-                valueLabelFormat={labelFormat}
-                defaultValue={fontSize}
-                onChangeCommitted={handleFontSize}
-              />
-            </ListItemSecondaryAction>
-          </ListItem>
+          <SliderRow
+            divider
+            primary="사이트 전체 폰트 크기"
+            secondary="표시 설정에서 글자 크기 브라우저 기본 설정 필요"
+            min={8}
+            max={30}
+            valueLabelFormat={labelFormat}
+            valueLabelDisplay="auto"
+            value={fontSize}
+            action={$setFontSize}
+            opacityOnChange={0.6}
+          />
           <SwitchRow
             primary="다크모드 글작성 배경색 강제 픽스"
             secondary="다크모드에서 글작성 배경색이 흰색으로 뜨는 문제를 수정합니다."
