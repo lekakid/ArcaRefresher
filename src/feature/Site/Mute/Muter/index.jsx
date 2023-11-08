b15ad6b52a186d5ac2a42c590ab8bf9a433db6ac
LeKAKiD <lekakid@kakao.com>
2023-11-08 16:57:13 +0900

    [Mute] 사이드바 뮤트 추가

--- src/feature/Site/Mute/Muter/index.jsx
+++ src/feature/Site/Mute/Muter/index.jsx
@@ -1,4 +1,5 @@
 export { default as BoardMuter } from './BoardMuter';
+export { default as SidebarMuter } from './SidebarMuter';
 export { default as ArticleMuter } from './ArticleMuter';
 export { default as CommentMuter } from './CommentMuter';
 export { default as ToastMuter } from './ToastMuter';
