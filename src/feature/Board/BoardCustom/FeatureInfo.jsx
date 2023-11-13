c973909ab5280761972357710d535c7859b0353b
LeKAKiD <lekakid@kakao.com>
2023-11-13 13:52:24 +0900

    [BoardCustom] QuickScrap 기능 통합, 새 창으로 열기 추가

---
+++ src/feature/Board/BoardCustom/FeatureInfo.jsx
@@ -0,0 +1,8 @@
+import 'core/types';
+
+/** @type {FeatureInfo} */
+export default {
+  ID: 'BoardCustom',
+  name: '게시판 커스텀',
+  description: '게시판 맞춤 기능을 제공합니다.',
+};
--- src/feature/Board/QuickScrap/FeatureInfo.jsx
+++
@@ -1,8 +0,0 @@
-import 'core/types';
-
-/** @type {FeatureInfo} */
-export default {
-  ID: 'QuickScrap',
-  name: '퀵 스크랩',
-  description: '우클릭 스크랩 메뉴를 추가합니다.',
-};
