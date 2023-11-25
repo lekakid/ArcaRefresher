8c84ab086a6c123ba41210591502c9fc5366c4b4
LeKAKiD <lekakid@kakao.com>
2023-11-25 17:04:06 +0900

    [env] 디버그 시 React 에러 트레이스 문제 수정

--- src/index.jsx
+++ src/index.jsx
@@ -2,6 +2,13 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
 
+/**
+ * Tampermonkey 환경 console은 구체적인 이유는 모르겠지만 defineProperties로 console을 재정의할 수 없음
+ * unsafeWindow의 console을 가져와서 React Error Trace가 대체되지 않는 문제를 해결함
+ */
+if (process.env.NODE_ENV !== 'production')
+  window.console = unsafeWindow.console;
+
 const appContainer = document.createElement('div');
 document.body.append(appContainer);
 
