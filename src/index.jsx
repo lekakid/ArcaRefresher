83828d2c3d6c3880ec974016edd6c3861588b548
LeKAKiD <lekakid@kakao.com>
2023-11-25 17:32:32 +0900

    [env] console 대체 코드 production 에도 반영

--- src/index.jsx
+++ src/index.jsx
@@ -3,11 +3,11 @@ import ReactDOM from 'react-dom';
 import App from './App';
 
 /**
- * Tampermonkey 환경 console은 구체적인 이유는 모르겠지만 defineProperties로 console을 재정의할 수 없음
+ * userscript의 격리된 window.console은 defineProperties로 재정의할 수 없는걸로 보임
  * unsafeWindow의 console을 가져와서 React Error Trace가 대체되지 않는 문제를 해결함
+ * useEffect 등에서 에러가 날 때 스크립트 전체가 뻗는게 이상한 것이었던 걸로 확인됨...
  */
-if (process.env.NODE_ENV !== 'production')
-  window.console = unsafeWindow.console;
+window.console = unsafeWindow.console;
 
 const appContainer = document.createElement('div');
 document.body.append(appContainer);
