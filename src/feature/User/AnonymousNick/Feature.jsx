b2dd062bd3eb1fd564f74912465fe868ade530d8
LeKAKiD <lekakid@kakao.com>
2023-11-27 11:17:07 +0900

    [AnonymousNick] 댓글 새로고침 버그 수정

--- src/feature/User/AnonymousNick/Feature.jsx
+++ src/feature/User/AnonymousNick/Feature.jsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from 'react';
+import React, { useEffect, useRef, useState } from 'react';
 import { useSelector } from 'react-redux';
 import { GlobalStyles } from '@mui/material';
 
@@ -6,6 +6,7 @@ import { ARTICLE_LOADED, ARTICLE_USER_INFO } from 'core/selector';
 import { getUserID, getUserKey } from 'func/user';
 import { useLoadChecker } from 'hooks/LoadChecker';
 
+import { EVENT_COMMENT_REFRESH, useEvent } from 'hooks/Event';
 import Info from './FeatureInfo';
 import Label from './Label';
 
@@ -22,42 +23,51 @@ const anonymousNickStyles = (
 );
 
 function AnonymousNick() {
-  const { storage, show } = useSelector((state) => state[Info.ID]);
-  const [nickData, setNickData] = useState([]);
   const articleLoaded = useLoadChecker(ARTICLE_LOADED);
+  const [addEventListener, removeEventListener] = useEvent();
+
+  const { storage, show } = useSelector((state) => state[Info.ID]);
+  const nickContainer = useRef([]);
+  const [nickData, setNickData] = useState([]);
 
   useEffect(() => {
-    if (!articleLoaded) return;
+    if (!articleLoaded) return undefined;
 
     const nickList = storage.prefixList.reduce((acc, p) => {
       acc.push(...storage.suffixList.map((s) => `${p} ${s}`));
       return acc;
     }, []);
     nickList.sort(() => Math.random() - 0.5);
-
-    let count = 0;
     const nickTable = {};
-    const data = [...document.querySelectorAll(ARTICLE_USER_INFO)].map(
-      (e, index) => {
-        const key = getUserKey(e, index);
-        const id = getUserID(e);
-        const nick =
-          nickTable[id] ||
-          (nickTable[id] =
-            nickList.pop() ||
-            `${storage.extraPrefix}${String((count += 1)).padStart(3, '0')}`);
-        const container =
-          e.querySelector('.anonymous') || document.createElement('span');
-        if (!container.parentNode) {
-          container.classList.add('anonymous');
-          e.insertAdjacentElement('afterend', container);
-        }
 
-        return { key, nick, container };
-      },
-    );
-    setNickData(data);
-  }, [articleLoaded, storage]);
+    const handler = () => {
+      let count = 0;
+      const data = [...document.querySelectorAll(ARTICLE_USER_INFO)].map(
+        (e, index) => {
+          const key = getUserKey(e, index);
+          const id = getUserID(e);
+          const nick =
+            nickTable[id] ||
+            (nickTable[id] =
+              nickList.pop() ||
+              `${storage.extraPrefix}${String((count += 1)).padStart(3, '0')}`);
+          const container =
+            nickContainer.current[index] || document.createElement('span');
+          if (!container.classList.contains('anonymous')) {
+            container.classList.add('anonymous');
+            nickContainer.current.push(container);
+          }
+          e.insertAdjacentElement('afterend', container);
+
+          return { key, nick, container };
+        },
+      );
+      setNickData(data);
+    };
+    handler();
+    addEventListener(EVENT_COMMENT_REFRESH, handler);
+    return () => removeEventListener(EVENT_COMMENT_REFRESH, handler);
+  }, [articleLoaded, storage, addEventListener, removeEventListener]);
 
   if (!show) return null;
   return (
