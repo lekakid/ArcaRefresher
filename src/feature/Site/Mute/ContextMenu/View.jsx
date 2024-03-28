72ede26d6551bd5257eec796f5d8be999828a3e6
LeKAKiD <lekakid@kakao.com>
2024-03-29 01:58:11 +0900

    [Mute] 카테고리 우클릭 메뉴 지원

--- src/feature/Site/Mute/ContextMenu/View.jsx
+++ src/feature/Site/Mute/ContextMenu/View.jsx
@@ -12,6 +12,8 @@ import {
 import { useContextMenu } from 'menu/ContextMenu';
 import { useSnackbarAlert } from 'menu/SnackbarAlert';
 import { ArcaUser } from 'func/user';
+import { getQuery } from 'func/http';
+import { useContent } from 'hooks/Content';
 
 import {
   getEmoticonList,
@@ -19,7 +21,13 @@ import {
   getBundleName,
   trimEmotURL,
 } from '../func';
-import { $addEmoticon, $addUser, $removeEmoticon, $removeUser } from '../slice';
+import {
+  $addEmoticon,
+  $addUser,
+  $removeEmoticon,
+  $removeUser,
+  $setCategoryConfig,
+} from '../slice';
 import Info from '../FeatureInfo';
 
 function makeRegex(id) {
@@ -28,41 +36,47 @@ function makeRegex(id) {
 
 function ContextMenu({ target }) {
   const dispatch = useDispatch();
-  const { user, contextRange } = useSelector((state) => state[Info.id].storage);
-  let contextSelector;
+  const { channel } = useContent();
+  const {
+    contextRange,
+    user,
+    category: { [channel.id]: category },
+  } = useSelector((state) => state[Info.id].storage);
+  let userSelector;
   switch (contextRange) {
     case 'articleItem':
-      contextSelector = `${BOARD_ITEMS_WITH_NOTICE}, ${USER_INFO}, ${USER_MENTION}`;
+      userSelector = `${BOARD_ITEMS_WITH_NOTICE}, ${USER_INFO}, ${USER_MENTION}`;
       break;
     case 'nickname':
-      contextSelector = `${USER_INFO}, ${USER_MENTION}`;
+      userSelector = `${USER_INFO}, ${USER_MENTION}`;
       break;
     default:
       console.warn('[Mute] contextRange 값이 올바르지 않음');
-      contextSelector = `${USER_INFO}, ${USER_MENTION}`;
+      userSelector = `${USER_INFO}, ${USER_MENTION}`;
       break;
   }
 
   const setSnack = useSnackbarAlert();
   const emotSelector =
     '[class$="emoticon"], .emoticon-wrapper > span, .article-body a.muted';
+  const categorySelector = '.board-category .item a';
   const [data, closeMenu] = useContextMenu(
     {
       key: Info.id,
-      selector: `${contextSelector}, ${emotSelector}`,
+      selector: `${userSelector}, ${emotSelector}, ${categorySelector}`,
       dataExtractor: () => {
         if (!target) return undefined;
 
-        if (target.matches(contextSelector)) {
+        if (target.matches(userSelector)) {
           let userElement = target;
           if (target.matches('.vrow')) {
             userElement = target.querySelector('span.user-info');
           }
           if (!userElement) return undefined;
 
-          const regex = makeRegex(new ArcaUser(userElement).toUID());
-          const exist = user.includes(regex);
-          return { type: 'user', regex, exist };
+          const userTmp = new ArcaUser(userElement);
+          const uid = userTmp.toUID();
+          return { type: 'user', uid };
         }
 
         if (target.matches(emotSelector)) {
@@ -85,13 +99,26 @@ function ContextMenu({ target }) {
           };
         }
 
+        if (target.matches(categorySelector)) {
+          const id = decodeURI(
+            getQuery(target.search).category || '글머리없음',
+          );
+          const label =
+            id !== '글머리없음' ? target.textContent.trim() : '글머리없음';
+          return {
+            type: 'category',
+            id,
+            label,
+          };
+        }
+
         return undefined;
       },
     },
-    [target, user],
+    [target],
   );
 
-  const handleBundleMute = useCallback(() => {
+  const handleMuteEmotBundle = useCallback(() => {
     (async () => {
       try {
         let { bundleId } = data;
@@ -133,7 +160,7 @@ function ContextMenu({ target }) {
     })();
   }, [closeMenu, data, dispatch, setSnack]);
 
-  const handleSingleMute = useCallback(() => {
+  const handleMuteSingleEmot = useCallback(() => {
     (async () => {
       try {
         let { bundleId, emotId } = data;
@@ -170,7 +197,7 @@ function ContextMenu({ target }) {
     })();
   }, [closeMenu, data, dispatch, setSnack]);
 
-  const handleBundleUnmute = useCallback(() => {
+  const handleUnmuteEmotBundle = useCallback(() => {
     (async () => {
       try {
         let { bundleId } = data;
@@ -196,7 +223,7 @@ function ContextMenu({ target }) {
     })();
   }, [closeMenu, data, dispatch, setSnack]);
 
-  const handleSingleUnmute = useCallback(() => {
+  const handleUnmuteSingleEmot = useCallback(() => {
     (async () => {
       try {
         let { bundleId } = data;
@@ -224,25 +251,45 @@ function ContextMenu({ target }) {
     })();
   }, [closeMenu, data, dispatch, setSnack]);
 
-  const handleMute = useCallback(() => {
-    const { regex, exist } = data;
+  const handleUser = useCallback(() => {
+    const { uid } = data;
+    const regex = makeRegex(uid);
+    const exist = user.includes(uid);
 
     dispatch(exist ? $removeUser(regex) : $addUser(regex));
     closeMenu();
-  }, [data, dispatch, closeMenu]);
+  }, [data, user, dispatch, closeMenu]);
+
+  const handleCategory = useCallback(
+    (type) => () => {
+      const config = category[data.id];
+      dispatch(
+        $setCategoryConfig({
+          channel: channel.id,
+          category: data.id,
+          config: {
+            ...config,
+            [type]: !config?.[type],
+          },
+        }),
+      );
+      closeMenu();
+    },
+    [data, category, channel, dispatch, closeMenu],
+  );
 
   if (data?.type === 'emoticon') {
     return (
       <>
         {!data.muted && (
           <List>
-            <MenuItem onClick={handleBundleMute}>
+            <MenuItem onClick={handleMuteEmotBundle}>
               <ListItemIcon>
                 <Block />
               </ListItemIcon>
               <Typography>아카콘 묶음 뮤트</Typography>
             </MenuItem>
-            <MenuItem onClick={handleSingleMute}>
+            <MenuItem onClick={handleMuteSingleEmot}>
               <ListItemIcon>
                 <Block />
               </ListItemIcon>
@@ -252,13 +299,13 @@ function ContextMenu({ target }) {
         )}
         {data.muted && (
           <List>
-            <MenuItem onClick={handleBundleUnmute}>
+            <MenuItem onClick={handleUnmuteEmotBundle}>
               <ListItemIcon>
                 <Block />
               </ListItemIcon>
               <Typography>아카콘 묶음 뮤트 해제</Typography>
             </MenuItem>
-            <MenuItem onClick={handleSingleUnmute}>
+            <MenuItem onClick={handleUnmuteSingleEmot}>
               <ListItemIcon>
                 <Block />
               </ListItemIcon>
@@ -271,12 +318,39 @@ function ContextMenu({ target }) {
   }
 
   if (data?.type === 'user') {
+    const exist = user.includes(data.uid);
     return (
       <List>
-        <MenuItem onClick={handleMute}>
-          <ListItemIcon>{data.exist ? <Redo /> : <Block />}</ListItemIcon>
+        <MenuItem onClick={handleUser}>
+          <ListItemIcon>{exist ? <Redo /> : <Block />}</ListItemIcon>
+          <Typography>{exist ? '사용자 뮤트 해제' : '사용자 뮤트'}</Typography>
+        </MenuItem>
+      </List>
+    );
+  }
+
+  if (data?.type === 'category') {
+    const config = category[data.id];
+
+    return (
+      <List>
+        <MenuItem sx={{ opacity: '1 !important' }} dense disabled>
+          {data.label}
+        </MenuItem>
+        <MenuItem onClick={handleCategory('mutePreview')}>
+          <ListItemIcon>
+            {config?.mutePreview ? <Redo /> : <Block />}
+          </ListItemIcon>
           <Typography>
-            {data.exist ? '사용자 뮤트 해제' : '사용자 뮤트'}
+            {config?.mutePreview ? '미리보기 뮤트 해제' : '미리보기 뮤트'}
+          </Typography>
+        </MenuItem>
+        <MenuItem onClick={handleCategory('muteArticle')}>
+          <ListItemIcon>
+            {config?.muteArticle ? <Redo /> : <Block />}
+          </ListItemIcon>
+          <Typography>
+            {config?.muteArticle ? '게시물 뮤트 해제' : '게시물 뮤트'}
           </Typography>
         </MenuItem>
       </List>
