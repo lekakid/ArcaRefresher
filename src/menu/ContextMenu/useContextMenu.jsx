b46f65b48e6775b8647d292e5fb45017481d690e
LeKAKiD <lekakid@kakao.com>
2024-03-31 15:17:17 +0900

    [ContextMenu] useContextMenu 코멘트 수정

--- src/menu/ContextMenu/useContextMenu.jsx
+++ src/menu/ContextMenu/useContextMenu.jsx
@@ -5,8 +5,10 @@ import { addTrigger, removeTrigger } from './slice';
 /**
  * 컨텍스트 메뉴를 등록함
  *
- * @param {string} key              구분 키(보통 FeatureInfo.id)
- * @param {string} selector         클릭된 엘리먼트를 기준으로 실제로 찾을 엘리먼트
+ * @param {string}   props.key            구분 키(보통 FeatureInfo.id)
+ * @param {string}   props.selector       클릭된 엘리먼트를 기준으로 실제로 찾을 엘리먼트
+ * @param {function} props.dataExtractor  클릭된 엘리먼트에서 데이터를 추출하는 함수
+ * @param {Array}    deps                 함수를 재등록할 디펜던시
  * @returns
  */
 export default function useContextMenu({ key, selector, dataExtractor }, deps) {
