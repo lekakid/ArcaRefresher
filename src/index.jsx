a7afb5ee042dc9cf904b41e63633a89eff526fe5
green1052 <green1052@pm.me>
2024-04-08 02:19:17 +0900

    document-body 사용

--- src/index.jsx
+++ src/index.jsx
@@ -7,27 +7,7 @@ import App from './App';
  */
 window.console = unsafeWindow.console;
 
-const appendAppContainer = () => {
-  const appContainer = document.createElement('div');
-  document.body.append(appContainer);
-  const root = createRoot(appContainer);
-
-  root.render(<App />);
-};
-
-if (!document.body) {
-  // @run-at document-start은 document.body가 없을 때 실행될 수 있음
-  // MutationObserver로 body가 추가되는 것을 감지하여 appendAppContainer를 실행
-  const mutationObserver = new MutationObserver((_, obs) => {
-    if (document.body) {
-      appendAppContainer();
-      obs.disconnect();
-    }
-  });
-  mutationObserver.observe(document.documentElement, {
-    childList: true,
-    subtree: true,
-  });
-} else {
-  appendAppContainer();
-}
+const appContainer = document.createElement('div');
+document.body.append(appContainer);
+const root = createRoot(appContainer);
+root.render(<App />);
