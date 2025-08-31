7fe9bb4fa84d8a6bc37424536d88941ba6ed6fa9
LeKAKiD <lekakid@kakao.com>
2025-08-31 15:04:20 +0900

    [Mute] 비로그인 상태에서 뮤트 시 번들 이름이 로그인으로 표시되는 문제 수정

--- src/feature/Site/Mute/func/getBundleName.jsx
+++ src/feature/Site/Mute/func/getBundleName.jsx
@@ -3,7 +3,11 @@ import { getDocument } from 'func/http';
 export default async function getBundleName(bundleId) {
   const response = await fetch(`/e/${bundleId}`);
   if (!response.ok) {
-    return `삭제된 이모티콘 - ${bundleId}`;
+    return `삭제된 이모티콘 (${bundleId})`;
+  }
+
+  if (response.url.includes('/u/login')) {
+    return `이모티콘 번들 (${bundleId})`;
   }
 
   const text = await response.text();
