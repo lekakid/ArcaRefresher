46d04f3d091e68a3afee66362ccec3244fde5228
LeKAKiD <lekakid@kakao.com>
2025-11-09 20:51:19 +0900

    [github-workflows] 버전 커밋을 체크하도록 개선

--- build/deploy.sh
+++ build/deploy.sh
@@ -4,10 +4,10 @@ set -e
 
 branch=$(git branch --show-current)
 version=$(npm --no-git-tag-version version $1 $2)
-git commit -am "[package] $version"
+git commit -am "[version] $version"
 
 if [ "$1" != "preminor" ] ; then
   git checkout master
-  git merge $branch -m $version --no-ff
+  git merge $branch -m "[version] $version" --no-ff
   git checkout develop
 fi
